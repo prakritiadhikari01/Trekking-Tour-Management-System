da69085eaf016beb0241adff7fa1a8cd29132a5b
Smarika Tripathi <smarikatripathi1@gmail.com>
2026-05-18 13:43:33 +0545

    adding booking system

---
+++ trekking_and_tour_management_system/trekking_and_tour_management_system/bookings/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Booking
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("user", "package", "status", "payment_status", "created_at")
+    list_filter = ("status", "payment_status")
+    search_fields = ("user__email", "package__title")
