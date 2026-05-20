e46222348ba5ea168f3152ab4b5a49e83aef799a
prakritiadhikari01 <prakriti.adhikari.456@gmail.com>
2026-05-20 12:57:55 +0545

    Before Guide app

--- trekking_and_tour_management_system/guide/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GuideConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = "trekking_and_tour_management_system.guide"
