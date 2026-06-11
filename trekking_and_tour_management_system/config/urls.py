f0027ad7d9f9998f0e2e63781e28912a235419c1
Smarika Tripathi <smarikatripathi1@gmail.com>
2026-06-11 14:57:46 +0545

    congig/ urls.py

--- trekking_and_tour_management_system/config/urls.py
+++ trekking_and_tour_management_system/config/urls.py
@@ -1,3 +1,4 @@
+import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -78,7 +79,7 @@ if settings.DEBUG:
         path("500/", default_views.server_error),
     ]
     if "debug_toolbar" in settings.INSTALLED_APPS:
-        import debug_toolbar
+        
 
         urlpatterns = [
             path("__debug__/", include(debug_toolbar.urls)),
